05a7dc158c9af0633960c69b96bb7ec9165f3c4d
s1weyt <vaxromeev2017@bk.ru>
2024-12-02 22:34:22 +0300

    Update graph_2.py

--- graph_2.py
+++ graph_2.py
@@ -1,13 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+print("строим график")
 def f(x):
     return x**2
 
-x = np.linspace(0, 5, 100)
-y = f(x)
+def bld():
+    x = np.linspace(-5, 5, 100)
+    y = f(x)
 
+    plt.plot(x,y, '--r')
+    plt.title("x^2")
+    plt.minorticks_on()
+    plt.grid(which='major')
+    plt.grid(which='minor', linestyle = ':')
+    
+    plt.tight_layout()
+    return plt.show()
 
-print("строим график")
-plt.plot(x,y, '--r')
-plt.show()
+if __name__ == "__main__":
+    bld()
