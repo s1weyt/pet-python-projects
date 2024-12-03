9794c8a07a2def593e6c78593cf2a0badc3c600a
s1weyt <vaxromeev2017@bk.ru>
2024-12-03 22:30:47 +0300

    Update graph_3.py

--- graph_3.py
+++ graph_3.py
@@ -4,7 +4,13 @@ x = [1, 2, 3, 4, 5]
 y = [25, 32, 34, 20, 25]
 
 plt.plot(x, y, color='green', marker='o', markersize=5)
-plt.xlabel('Ось x') #Подпись для оси x
-plt.ylabel('Ось y') #Подпись для оси y
-plt.title('Первый график') #Название
+plt.xlabel('Ось x')  #Подпись для оси x
+plt.ylabel('Ось y')  #Подпись для оси y
+plt.title('Первый график')  #Название
+
+plt.minorticks_on()  # включаем отображение дополнительных отсечек сетки
+plt.grid(which = 'major')  # включаем основную сетку
+plt.grid(which = 'minor', linestyle = ':')  # включаем дополнительную сетку
+
+plt.tight_layout()  # включаем автоматическое красивое отображение
 plt.show()
