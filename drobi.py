d76222f53eee5f77a340e2287554a687eb53a17f
s1weyt <vaxromeev2017@bk.ru>
2024-12-06 22:28:15 +0300

    Create drobi.py

---
+++ drobi.py
@@ -0,0 +1,21 @@
+from fractions import Fraction
+
+a = Fraction(6,4)
+b = Fraction(7,12)
+c = a * b
+print(c)
+
+#Получение числителя/знаменателя
+print(c.numerator)
+print(c.denominator)
+
+#Конвертирование в float
+print(float(c))
+
+#Ограничение значения знаменателя
+print(c.limit_denominator(8))
+
+#Преобразование из float в дробь
+x = 5.75
+y = Fraction(*x.as_integer_ratio())
+print(y)
